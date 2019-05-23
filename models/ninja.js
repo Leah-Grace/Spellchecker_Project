2ec599d0f0c0495262a4c4da7cf3a35e4c00d9c1
Leah-Grace <leahgdg@hotmail.com>
2019-05-23 18:49:23 -0400

    included geoJSON

--- models/ninja.js
+++ models/ninja.js
@@ -1,6 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/*
+  "geometry": {
+    "type": "Point",
+    "coordinates": [125.6, 10.1]
+  }
+*/
+//create geolocation Schema
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        deafult: "Point",
+    },
+    coordinates: {
+        type: [Numbers],
+        index: "2dsphere"
+    },
+
+});
+
+
+
+
 const NinjaSchema = new Schema({
     name: {
         type: String,
@@ -12,10 +34,13 @@ const NinjaSchema = new Schema({
     available: {
         type: Boolean,
         default: false
-    }
+    },
     //add geolocation
+geometry: GeoSchema
+
 });
 
 const Ninja = mongoose.model('ninja', NinjaSchema);
 
 module.exports = Ninja;
+
