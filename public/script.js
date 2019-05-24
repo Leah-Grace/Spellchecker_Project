67d3ce23692e16be6cbe84112f95a2afa4b2aed8
Leah-Grace <leahgdg@hotmail.com>
2019-05-24 18:11:40 -0400

    fixed broked logic on client-side. script.js success

--- public/script.js
+++ public/script.js
@@ -1,14 +1,21 @@
 /*global fetch*/
 function handleClick(){
-  const name = document.getElementById("name").value;
+  const name = document.getElementById("getuserInfo").value;
   
   console.log(name);
   fetch("/getemail" + "?" +"name=" + name)
     .then(response=>response.text())
     .then(function(response){
       console.log(response);
-      document.getElementById("results").innerHTML = response;
+      document.getElementById("showEmail").innerHTML = response;
     });
-  
-  
+};
+
+// function POSTUserInfo(){
+//   const newUserName = document.getElementById("user-name").value;
+//   const newUserId = document.getElementById("user-id").value;
+//   const newUserMessage = document.getElementById("message").value;
+//   console.log(newUserName, newUserId, newUserMessage);
+
+  fetch("POSTREQROUTE" + newUserName + newUserId + message)
 }
